d5fe74945cf674b13dc33aafc0d54f2d5fb64313
bulutluoz <abbulutluoz@gmail.com>
2024-07-12 12:41:14 +0200

    day03 sonu

--- src/day03_locators/C04_XPath.java
+++ src/day03_locators/C04_XPath.java
@@ -1,16 +1,46 @@
 package day03_locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+import java.util.List;
+
 public class C04_XPath {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("Webdriver.chrome.driver","KurulumDosyalari/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // testotomasyonu anasayfaya gidin
+        driver.get("https://www.testotomasyonu.com");
 
         // dress icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("dress" + Keys.ENTER);
 
         // urun bulunabildigini test edin
 
+        List<WebElement> urunIsimElementleriList = driver.findElements(By.xpath("//a[@class = 'prod-title mb-3 ']"));
+
+        if (urunIsimElementleriList.size() > 0){
+            System.out.println("urun bulma testi PASSED");
+        }else System.out.println("urun bulma testi FAILED");
+
         // bulunan urun isimlerini yazdirin
 
+        System.out.println(ReusableMethods.getStringList(urunIsimElementleriList));
+
+
         // sayfayi kapatin
+        Thread.sleep(3000);
+        driver.quit();
+
     }
 }
---
+++ src/day03_locators/C05_XPath.java
@@ -0,0 +1,13 @@
+package day03_locators;
+
+public class C05_XPath {
+
+    public static void main(String[] args) {
+
+        //1- https://testotomasyonu.com/addremove/ adresine gidin
+        //2- Add Element butonuna basin
+        //3- Remove butonu’nun gorunur oldugunu test edin
+        //4- Remove tusuna basin
+        //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+    }
+}
