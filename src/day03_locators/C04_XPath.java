dc1302ff1fba8fbef493b987b9dd9119c09f36e6
bulutluoz <abbulutluoz@gmail.com>
2024-07-12 12:27:59 +0200

    day03 sonu

---
+++ src/day03_locators/C04_XPath.java
@@ -0,0 +1,16 @@
+package day03_locators;
+
+public class C04_XPath {
+    public static void main(String[] args) {
+
+        // testotomasyonu anasayfaya gidin
+
+        // dress icin arama yapin
+
+        // urun bulunabildigini test edin
+
+        // bulunan urun isimlerini yazdirin
+
+        // sayfayi kapatin
+    }
+}
