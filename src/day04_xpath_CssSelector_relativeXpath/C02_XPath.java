e70d75bfb08f2dac4296848ad17da40a914873e9
bulutluoz <abbulutluoz@gmail.com>
2024-07-15 10:44:48 +0200

    day04 sonu

--- src/day04_xpath_CssSelector_relativeXpath/C02_XPath.java
+++ src/day04_xpath_CssSelector_relativeXpath/C02_XPath.java
@@ -1,12 +1,17 @@
 package day04_xpath_CssSelector_relativeXpath;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C02_XPath {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //1-  bir class olusturun
         System.setProperty("Webdriver.chrome.driver","KurulumDosyalari/chromedriver");
@@ -16,7 +21,7 @@ public class C02_XPath {
         //2-  https://www.testotomasyonu.com/ adresine gidin
         driver.get("https://www.testotomasyonu.com/");
         //3-  Browseri tam sayfa yapin
-        driver.manage().window().fullscreen();
+        driver.manage().window().maximize();
         //4-  Sayfayi “refresh” yapin
         driver.navigate().refresh();
         //5-  Sayfa basliginin “Test Otomasyonu” ifadesi icerdigini test edin
@@ -29,14 +34,59 @@ public class C02_XPath {
         }else System.out.println("Title testi FAILED");
 
         //6-  Furniture linkine tiklayin
-
+        driver.findElement(By.xpath("(//a[text()='Furniture'])[3]"))
+                .click();
         //7-  price range filtresinde min degere 40, max degere 200 yazip filtreleyin
 
+        WebElement minKutusu = driver.findElement(By.xpath("//input[@name='min']"));
+        WebElement maxKutusu = driver.findElement(By.xpath("//input[@name='max']"));
+        WebElement filtreleButonu = driver.findElement(By.xpath("//*[@name='button']"));
+
+
+        minKutusu.clear();
+        maxKutusu.clear();
+        minKutusu.sendKeys("40");
+        maxKutusu.sendKeys("200");
+
+        Thread.sleep(2000);
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+
+        Thread.sleep(2000);
+        filtreleButonu.click();
+
+
         //8-  filtreleme sonucunda urun bulunabildigini test edin
+        List<WebElement> bulunanUrunElementleriList =
+                driver.findElements(By.xpath("//div[@class='product-box mb-2 pb-1']"));
+
+        if (bulunanUrunElementleriList.size() > 0){
+            System.out.println("Urun bulma testi PASSED");
+        }else System.out.println("Urun bulma testi FAILED");
 
         //10-Ilk urunu tiklayin
+        bulunanUrunElementleriList.get(0).click();
 
         //11- Urun fiyatinin 40 ile 200 arasinda oldugunu test edin
+        WebElement fiyatElementi = driver.findElement(By.id("priceproduct"));
+
+        String fiyatStr = fiyatElementi.getText(); // $50.00
+
+        fiyatStr = fiyatStr.replaceAll("\\D",""); // "5000"
+
+        double fiyatDouble = Double.parseDouble(fiyatStr); // double 5000
+
+        fiyatDouble /= 100 ; // double 50.00
+
+        if (fiyatDouble >=40 && fiyatDouble<=200){
+            System.out.println("ilk urun fiyat testi PASSED");
+        }else System.out.println("ilk urun fiyat testi FAILED");
+
+
+
         //12-Sayfayi kapatin
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
